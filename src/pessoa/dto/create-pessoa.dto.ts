02b6316ccea6f4ba18534132493ab59c2f297b4b
renanjava <renancvr777@gmail.com>
2025-04-15 20:25:54 -0300

    feat: validações do DTO

--- src/pessoa/dto/create-pessoa.dto.ts
+++ src/pessoa/dto/create-pessoa.dto.ts
@@ -1,7 +1,13 @@
+import { IsString, IsUUID } from 'class-validator';
 import { PessoaEntity } from '../entities/pessoa.entity';
 
 export class CreatePessoaDto implements PessoaEntity {
+  @IsString()
   nome: string;
+
+  @IsString()
   cpf: string;
+
+  @IsUUID()
   trabalhoId: string;
 }
--- src/trabalho/dto/create-trabalho.dto.ts
+++ src/trabalho/dto/create-trabalho.dto.ts
@@ -1,6 +1,10 @@
+import { IsString } from 'class-validator';
 import { TrabalhoEntity } from '../entities/trabalho.entity';
 
 export class CreateTrabalhoDto implements TrabalhoEntity {
+  @IsString()
   nome: string;
+
+  @IsString()
   descricao: string;
 }
